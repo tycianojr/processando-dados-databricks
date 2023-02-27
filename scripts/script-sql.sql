30d44fc367792b9f7fbe4fd5b3572135d522e376
tycianojr <dev.tyciano@gmail.com>
2023-02-26 23:21:38 -0300

    Adicionando scripts

---
+++ scripts/script-sql.sql
@@ -0,0 +1,12 @@
+senha udemy: 65T$W638lgg6
+
+
+
+- Mapeamento dos processos de scoragem e priorização, para os dados de contato (endereço, e-mail, telefone fixo e telefone celular);
+- Ajustar os processos de scoragem e priorização com base no levantamento realizado;
+- Correção dos GAP's entre as posições;
+- Inclusão no processo de priorização a informação de recência;
+- Reescrita do processo de carga da Visão Única (PF/PJ) – Última etapa da carga do Datacare;
+- Atualização das Blacklists de Nomes, Telefones, E-mails, Domínios de e-mails e outras ;
+- Levantamento de pessoas físicas a excluir da Visão Única/Insumo Hot (2.542.495 CPFs).
+
---
+++ scripts/spark.py
@@ -0,0 +1,24 @@
+﻿# Analytics
+
+
+# Lista o conteúdo
+dbutils.fs.ls("s3://learning-databricks-thsaj/dados/dbadmin/")
+
+# Carrega os dados em um dataframe do Spark
+df = spark.read.load("s3://learning-databricks-thsaj/dados/dbadmin/thsaj_tb_usuarios/")
+
+# Visualiza os dados
+display(df) 
+
+# Executa query SQL
+%sql
+SELECT nome_usuario, nome_cidade
+FROM dbadmin.thsaj_tb_usuarios, dbadmin.thsaj_tb_cidades
+WHERE dbadmin.thsaj_tb_cidades.codigo_cidade = dbadmin.thsaj_tb_usuarios.cod_cidade;
+
+# Executa query SQL
+%sql
+SELECT nome_cidade, COUNT(*)
+FROM dbadmin.thsaj_tb_usuarios, dbadmin.thsaj_tb_cidades
+WHERE dbadmin.thsaj_tb_cidades.codigo_cidade = dbadmin.thsaj_tb_usuarios.cod_cidade
+GROUP BY nome_cidade;
